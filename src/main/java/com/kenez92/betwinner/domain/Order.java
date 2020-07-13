ac9d611476b5348f50531b5e67dc3c63bf347db3
Daniel Kaminski <kenez92@gmail.com>
2020-07-13 20:25:36 +0200

    Order Controller, repository, service, mapper

---
+++ src/main/java/com/kenez92/betwinner/domain/Order.java
@@ -0,0 +1,19 @@
+package com.kenez92.betwinner.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "\"ORDER\"")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+}
---
+++ src/main/java/com/kenez92/betwinner/domain/OrderDto.java
@@ -0,0 +1,14 @@
+package com.kenez92.betwinner.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDto {
+    private Long id;
+}
