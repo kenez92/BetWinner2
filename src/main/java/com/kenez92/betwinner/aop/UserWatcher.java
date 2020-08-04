82ddcf559e89c39296408b760ab5e38f341d4383
Daniel Kaminski <kenez92@gmail.com>
2020-08-04 17:23:34 +0200

    Fix annotation in UserWatcher

--- src/main/java/com/kenez92/betwinner/aop/UserWatcher.java
+++ src/main/java/com/kenez92/betwinner/aop/UserWatcher.java
@@ -3,6 +3,7 @@ package com.kenez92.betwinner.aop;
 import com.kenez92.betwinner.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import org.springframework.stereotype.Component;
 public class UserWatcher {
     private final UserService userService;
 
-    @Before("execution(* com.kenez92.betwinner.service.UserService.createUser(..))")
+    @AfterReturning("execution(* com.kenez92.betwinner.service.UserService.createUser(..))")
     public void logEvent() {
         Long quantityOfUsers = userService.getQuantityOfUsers();
         log.info("We have new user :) , now we have : " + quantityOfUsers + " users");
