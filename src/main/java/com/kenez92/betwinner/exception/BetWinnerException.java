030f2f59102f814ff1b3163893dfc3286dc82954
Daniel Kaminski <kenez92@gmail.com>
2020-08-22 12:33:17 +0200

    CouponTypeService implementation

--- src/main/java/com/kenez92/betwinner/exception/BetWinnerException.java
+++ src/main/java/com/kenez92/betwinner/exception/BetWinnerException.java
@@ -38,6 +38,7 @@ public class BetWinnerException extends RuntimeException {
     public static final String ERR_MATCH_SCORE_NOT_FOUND_EXCEPTION = "Match score not found!";
     public static final String ERR_MATCH_NOT_FOUND_EXCEPTION = "Match not found!";
     public static final String ERR_MATCH_DAY_NOT_FOUND_EXCEPTION = "Match day not found!";
+    public static final String ERR_COUPON_TYPE_NOT_FOUND_EXCEPTION = "Coupon type not found!";
     //openweathermap
     public static final String ERR_WEATHER_NULL_EXCEPTION = "Weather class is equal to null!";
     public static final String ERR_WEATHER_NOT_FOUND_EXCEPTION = "Weather not found!";
--- src/main/java/com/kenez92/betwinner/service/coupons/CouponTypeService.java
+++ src/main/java/com/kenez92/betwinner/service/coupons/CouponTypeService.java
@@ -1,10 +1,65 @@
 package com.kenez92.betwinner.service.coupons;
 
 import com.kenez92.betwinner.domain.coupons.CouponTypeDto;
+import com.kenez92.betwinner.entity.coupons.CouponType;
+import com.kenez92.betwinner.exception.BetWinnerException;
+import com.kenez92.betwinner.mapper.coupons.CouponTypeMapper;
+import com.kenez92.betwinner.repository.coupons.CouponTypeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class CouponTypeService {
+    private final CouponTypeRepository couponTypeRepository;
+    private final CouponTypeMapper couponTypeMapper;
+
     public CouponTypeDto getCouponType(Long couponId) {
+        log.debug("Getting coupon type id: {}", couponId);
+        CouponType couponType = couponTypeRepository.findById(couponId)
+                .orElseThrow(() -> new BetWinnerException(BetWinnerException.ERR_COUPON_TYPE_NOT_FOUND_EXCEPTION));
+        CouponTypeDto couponTypeDto = couponTypeMapper.mapToCouponTypeDto(couponType);
+        log.debug("Return coupon type: {}", couponTypeDto);
+        return couponTypeDto;
+    }
+
+    public List<CouponTypeDto> getCouponTypes() {
+        log.debug("Getting coupon types");
+        List<CouponType> couponTypeList = couponTypeRepository.findAll();
+        List<CouponTypeDto> couponTypeDtoList = couponTypeMapper.mapToCouponTypeDtoList(couponTypeList);
+        log.debug("Return coupon types: {}", couponTypeDtoList);
+        return couponTypeDtoList;
+    }
+
+    public CouponTypeDto createCouponType(CouponTypeDto couponTypeDto) {
+        log.debug("Creating coupon type: {}", couponTypeDto);
+        CouponType couponType = couponTypeRepository.save(couponTypeMapper.mapToCouponType(couponTypeDto));
+        CouponTypeDto savedCouponTypeDto = couponTypeMapper.mapToCouponTypeDto(couponType);
+        log.debug("Return created coupon type: {}", savedCouponTypeDto);
+        return savedCouponTypeDto;
+    }
+
+    public CouponTypeDto updateCouponType(CouponTypeDto couponTypeDto) {
+        log.debug("Updating coupon type: {}", couponTypeDto);
+        CouponType couponType = couponTypeRepository.save(couponTypeMapper.mapToCouponType(couponTypeDto));
+        CouponTypeDto updatedCouponType = couponTypeMapper.mapToCouponTypeDto(couponType);
+        log.debug("Return updated coupon type: {}", updatedCouponType);
+        return updatedCouponType;
+    }
+
+    public boolean deleteCouponType(Long couponTypeId) {
+        log.debug("Deleting coupon type id: {}", couponTypeId);
+        couponTypeRepository.deleteById(couponTypeId);
+        if (couponTypeRepository.existsById(couponTypeId)) {
+            log.debug("Coupon type not deleted id: {}", couponTypeId);
+            return false;
+        } else {
+            log.debug("Coupon type deleted id: {}", couponTypeId);
+            return true;
+        }
     }
 }
