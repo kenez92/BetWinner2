4e51fc8158e4dac1ffadc81a438e3cc9ffbaffd3
Daniel Kaminski <kenez92@gmail.com>
2020-10-29 21:17:10 +0100

    Add SaveCompetitionTable

--- src/main/java/com/kenez92/betwinner/service/scheduler/SaveDataSchedulerService.java
+++ src/main/java/com/kenez92/betwinner/service/scheduler/SaveDataSchedulerService.java
@@ -4,14 +4,18 @@ import com.kenez92.betwinner.domain.fotballdata.AvailableCompetitions;
 import com.kenez92.betwinner.domain.fotballdata.standings.FootballTable;
 import com.kenez92.betwinner.domain.table.CompetitionDto;
 import com.kenez92.betwinner.domain.table.CompetitionSeasonDto;
+import com.kenez92.betwinner.domain.table.CompetitionTableDto;
 import com.kenez92.betwinner.domain.table.CurrentMatchDayDto;
 import com.kenez92.betwinner.football.client.FootballClient;
+import com.kenez92.betwinner.service.scheduler.tables.SaveCompetitionTable;
 import com.kenez92.betwinner.service.scheduler.tables.SaveFootballCompetition;
 import com.kenez92.betwinner.service.scheduler.tables.SaveFootballCompetitionSeason;
 import com.kenez92.betwinner.service.scheduler.tables.SaveFootballCurrentMatchDay;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
@@ -22,19 +26,31 @@ public class SaveDataSchedulerService {
     private final SaveFootballCompetition saveFootballCompetition;
     private final SaveFootballCompetitionSeason saveFootballCompetitionSeason;
     private final SaveFootballCurrentMatchDay saveFootballCurrentMatchDay;
+    private final SaveCompetitionTable saveCompetitionTable;
 
     public void saveCompetitionData() throws InterruptedException {
         for (Long competitionId : AvailableCompetitions.availableCompetitionList) {
+            CompetitionDto competitionDto = null;
+            CompetitionSeasonDto competitionSeasonDto = null;
+            CurrentMatchDayDto currentMatchDayDto = null;
+
             TimeUnit.SECONDS.sleep(DELAY);
             FootballTable footballTable = footballClient.getTable(competitionId);
-            CompetitionDto competitionDto = saveFootballCompetition.saveCompetition(footballTable);
+            competitionDto = saveFootballCompetition.saveCompetition(footballTable);
             if (competitionDto != null) {
-                CompetitionSeasonDto competitionSeasonDto = saveFootballCompetitionSeason
+                competitionSeasonDto = saveFootballCompetitionSeason
                         .saveCompetitionSeason(footballTable.getSeason(), competitionDto);
-                if (competitionSeasonDto != null) {
-                    CurrentMatchDayDto currentMatchDayDto = saveFootballCurrentMatchDay
-                            .saveCurrentMatchDayDto(competitionSeasonDto, footballTable.getFootballStandings(),
-                                    footballTable.getSeason().getCurrentMatchday());
+            }
+            if (competitionSeasonDto != null) {
+                currentMatchDayDto = saveFootballCurrentMatchDay
+                        .saveCurrentMatchDayDto(competitionSeasonDto, footballTable.getFootballStandings(),
+                                footballTable.getSeason().getCurrentMatchday());
+            }
+            if (currentMatchDayDto != null) {
+                List<CompetitionTableDto> competitionTableDtoList = new ArrayList<>();
+                for (int i = 0; i < footballTable.getFootballStandings().length; i++) {
+                    competitionTableDtoList.add(saveCompetitionTable.saveCompetitionTable(currentMatchDayDto,
+                            footballTable.getFootballStandings()[i]));
                 }
             }
         }
