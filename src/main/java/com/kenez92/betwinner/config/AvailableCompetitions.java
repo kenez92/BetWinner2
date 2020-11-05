44d88038789ab8c28ceba1e18f8f615e620fcba5
Daniel <daniel.kaminski@smartive.app>
2020-11-05 16:28:53 +0100

    Back to java 8

---
+++ src/main/java/com/kenez92/betwinner/config/AvailableCompetitions.java
@@ -0,0 +1,24 @@
+package com.kenez92.betwinner.config;
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Component
+public class AvailableCompetitions {
+    private final List<Long> availableCompetitionList = new ArrayList<>();
+
+    public AvailableCompetitions() {
+        availableCompetitionList.add(2002L);          // GERMANY - BUNDESLIGA
+        availableCompetitionList.add(2003L);          // NETHERLAND - EREDIVISIE
+//        availableCompetitionList.add(2014L);          // SPAIN - LA_LIGA
+//        availableCompetitionList.add(2017L);          // PORTUGAL - PRIMEIRA_LIGA
+//        availableCompetitionList.add(2021L);          // ENGLAND - PREMIER_LEAGUE
+//        availableCompetitionList.add(2015L);          // FRANCE - LIGUE 1
+    }
+
+}
