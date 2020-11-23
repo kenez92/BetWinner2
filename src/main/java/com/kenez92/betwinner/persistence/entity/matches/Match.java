d5dfeeef5dd4fa463edf92d29c00d05b072cb86c
Daniel <daniel.kaminski@smartive.app>
2020-11-23 22:39:31 +0100

    Update Match entity

--- src/main/java/com/kenez92/betwinner/persistence/entity/matches/Match.java
+++ src/main/java/com/kenez92/betwinner/persistence/entity/matches/Match.java
@@ -1,15 +1,13 @@
 package com.kenez92.betwinner.persistence.entity.matches;
 
 import com.kenez92.betwinner.persistence.entity.coupons.CouponType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @NamedQueries({
         @NamedQuery(
@@ -26,7 +24,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Data
+@ToString
+@Getter
+@Setter
 @Entity
 @Table(name = "\"MATCH\"")
 public class Match {
@@ -35,22 +35,22 @@ public class Match {
     @Column(name = "ID")
     private Long id;
 
-    @Column(name = "FOOTBALL_ID")
+    @Column(name = "FOOTBALL_ID", nullable = false)
     private Long footballId;
 
-    @Column(name = "HOME_TEAM")
+    @Column(name = "HOME_TEAM", nullable = false)
     private String homeTeam;
 
-    @Column(name = "AWAY_TEAM")
+    @Column(name = "AWAY_TEAM", nullable = false)
     private String awayTeam;
 
-    @Column(name = "COMPETITION_ID")
+    @Column(name = "COMPETITION_ID", nullable = false)
     private Long competitionId;
 
-    @Column(name = "SEASON_ID")
+    @Column(name = "SEASON_ID", nullable = false)
     private Long seasonId;
 
-    @Column(name = "DATE")
+    @Column(name = "DATE", nullable = false)
     private Date date;
 
     @Column(name = "HOME_TEAM_POSITION_IN_TABLE")
@@ -92,4 +92,51 @@ public class Match {
             targetEntity = CouponType.class,
             mappedBy = "match")
     private List<CouponType> couponTypeList = new ArrayList<>();
+
+    public boolean equals(Match match) {
+        if (match == null) {
+            return false;
+        }
+        boolean result = true;
+        result &= footballId.equals(match.getFootballId());
+        result &= homeTeam.equals(match.getHomeTeam());
+        result &= awayTeam.equals(match.getAwayTeam());
+        result &= competitionId.equals(match.getCompetitionId());
+        result &= seasonId.equals(match.getSeasonId());
+        result &= date.getTime() == match.getDate().getTime();
+        if (match.getHomeTeamPositionInTable() != null) {
+            result &= homeTeamPositionInTable.equals(match.getHomeTeamPositionInTable());
+        }
+        if (match.getAwayTeamPositionInTable() != null) {
+            result &= awayTeamPositionInTable.equals(match.getAwayTeamPositionInTable());
+        }
+        if (match.getHomeTeamChance() != null) {
+            result &= homeTeamChance.equals(match.getHomeTeamChance());
+        }
+        if (match.getAwayTeamChance() != null) {
+            result &= awayTeamChance.equals(match.getAwayTeamChance());
+        }
+        if (match.getRound() != null) {
+            result &= round.equals(match.getRound());
+        }
+        if (match.getHomeTeamCourse() != null) {
+            result &= homeTeamCourse.equals(match.getHomeTeamCourse());
+        }
+        if (match.getDrawCourse() != null) {
+            result &= drawCourse.equals(match.getDrawCourse());
+        }
+        if (match.getAwayTeamCourse() != null) {
+            result &= awayTeamCourse.equals(match.getAwayTeamCourse());
+        }
+        if (match.getMatchDay() != null) {
+            result &= matchDay.equals(match.getMatchDay());
+        }
+        if (match.getMatchScore() != null) {
+            result &= matchScore.equals(match.getMatchScore());
+        }
+        if (match.getWeather() != null) {
+            result &= weather.equals(match.getWeather());
+        }
+        return result;
+    }
 }
