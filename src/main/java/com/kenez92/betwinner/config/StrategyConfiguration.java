e3c5913f4f324ba6e3ad00480ddd6bdc5f72d33b
Daniel <daniel.kaminski@smartive.app>
2021-03-10 09:47:44 +0100

    Add StrategyConfiguration

---
+++ src/main/java/com/kenez92/betwinner/config/StrategyConfiguration.java
@@ -0,0 +1,18 @@
+package com.kenez92.betwinner.config;
+
+public class StrategyConfiguration {
+    public final static double NORMAL_STRATEGY_FROM = 60d;
+    public final static double NORMAL_STRATEGY_TO = 75d;
+
+    public final static double AGGRESSIVE_STRATEGY_FROM = 50d;
+    public final static double AGGRESSIVE_STRATEGY_TO = 60d;
+
+    public final static double DEFENSIVE_STRATEGY_FROM = 70d;
+    public final static double DEFENSIVE_STRATEGY_TO = 100d;
+
+    public final static double EVERYTHING_STRATEGY_FROM = 0d;
+    public final static double EVERYTHING_STRATEGY_TO = 100d;
+
+    public final static double PERCENT_70_STRATEGY_FROM = 70d;
+    public final static double PERCENT_70_STRATEGY_TO = 70d;
+}
